0bd0dd6a7fcce7b49f9d7e414cecc0ab41e607ab
ltakoushi18 <ltakoushi18@gmail.com>
2025-10-11 13:08:46 +1000

    fixed id issue

--- src/java.js
+++ src/java.js
@@ -5,5 +5,5 @@ function searchCity(event) {
   cityChosenElement.innerHTML = citySearchInput.value;
 }
 
-let searchForm = document.querySelector("#search-form");
+let searchForm = document.querySelector("#search-bar");
 searchForm.addEventListener("submit", searchCity);
