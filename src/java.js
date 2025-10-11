1d0fe4085bb5e0b57bfb56022059b20049bbcb87
ltakoushi18 <ltakoushi18@gmail.com>
2025-10-11 15:30:20 +1000

    added weather data

--- src/java.js
+++ src/java.js
@@ -2,9 +2,39 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#temp-now");
   let temperature = response.data.temperature.current;
   let cityChosenElement = document.querySelector("#city-chosen");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let speedElement = document.querySelector("#speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityChosenElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  speedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function cityEntered(city) {
